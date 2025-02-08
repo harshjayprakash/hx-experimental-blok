a8479adca36b070c0005671ddb3be9d4c2a7361c
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-08 18:42:41 +0000

    feat: add context typedef structure

--- src/core/context.h
+++ src/core/context.h
@@ -1,4 +1,13 @@
 #ifndef __BLOK_CONTEXT_H_
 #define __BLOK_CONTEXT_H_
 
+#include <Windows.h>
+
+typedef struct Context__ {
+    HINSTANCE instanceHandle;
+    LPCWSTR commandLine;
+    DWORD showFlag;
+} Context;
+
+
 #endif // !__BLOK_CONTEXT_H_
