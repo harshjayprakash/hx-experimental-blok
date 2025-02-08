fa17c3894ea3bb0a5e246483b1177ded8683cc09
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-08 18:47:18 +0000

    feat: add get context helper function

--- src/core/module.c
+++ src/core/module.c
@@ -1,1 +1,7 @@
 #include "module.h"
+
+Context *BlokContextGet(void)
+{
+    static Context context;
+    return &context;
+}
--- src/core/module.h
+++ src/core/module.h
@@ -1,4 +1,8 @@
 #ifndef __BLOK_MODULE_H_
 #define __BLOK_MODULE_H_
 
+#include "context.h"
+
+Context *BlokContextGet(void);
+
 #endif // !__BLOK_MODULE_H_
