936785ad672d090acb33e95096c51cc069440b84
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-08 18:43:17 +0000

    feat: implement context functions

--- src/core/context.c
+++ src/core/context.c
@@ -1,1 +1,22 @@
 #include "context.h"
+
+void BlokContextInit(
+    Context *context, HINSTANCE instanceHandle, LPCWSTR commandLine, DWORD showFlag)
+{
+    if (!context) { return; }
+    if (!instanceHandle) { return; }
+
+    context->instanceHandle = instanceHandle;
+    context->commandLine = commandLine;
+    context->showFlag = showFlag;
+}
+
+void BlokContextRun(Context *context)
+{
+    if (!context) { return; }
+}
+
+void BlokContextTerminate(Context *context)
+{
+    if (!context) { return; }
+}
