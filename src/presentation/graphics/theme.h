d7b87fc2fd402d90d126f3331f1cbda7bd18ec4b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-09 11:32:23 +0000

    feat(ui-gfx): add theme enum

--- src/presentation/graphics/theme.h
+++ src/presentation/graphics/theme.h
@@ -1,4 +1,11 @@
 #ifndef __BLOK_THEME_H_
 #define __BLOK_THEME_H_
 
+typedef enum Theme__ {
+    BLOK_THEME_UNSET = 0,
+    BLOK_THEME_DARK = 1,
+    BLOK_THEME_LIGHT = 2,
+} Theme;
+
+
 #endif // !__BLOK_THEME_H_
