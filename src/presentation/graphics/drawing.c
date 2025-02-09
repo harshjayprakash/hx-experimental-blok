8cf741bcf5612ba22755a2f94025f39cda82e734
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-09 11:53:10 +0000

    feat(ui-gfx): add drawing lifecycle functions

--- src/presentation/graphics/drawing.c
+++ src/presentation/graphics/drawing.c
@@ -1,1 +1,24 @@
 #include "drawing.h"
+
+#define ___BLOK_FREE_WINGDI_OBJECT(winGdiObject) \
+    if (winGdiObject != NULL)                    \
+    {                                            \
+        (void) DeleteObject(winGdiObject);       \
+    }
+
+void BlokDrawingInit(Drawing *drawing, const SelectColours *colours)
+{
+    if (!drawing) { return; }
+    if (!colours) { return; }
+
+    drawing->backgroundBrush = CreateSolidBrush(colours->background);
+    drawing->foregroundBrush = CreateSolidBrush(colours->foreground);
+    drawing->accentBrush = CreateSolidBrush(colours->accent);
+}
+
+void BlokDrawingFree(Drawing *drawing)
+{
+    ___BLOK_FREE_WINGDI_OBJECT(drawing->backgroundBrush);
+    ___BLOK_FREE_WINGDI_OBJECT(drawing->foregroundBrush);
+    ___BLOK_FREE_WINGDI_OBJECT(drawing->accentBrush);
+}
--- src/presentation/graphics/drawing.h
+++ src/presentation/graphics/drawing.h
@@ -1,5 +1,7 @@
 #ifndef __BLOK_DRAWING_H_
 #define __BLOK_DRAWING_H_
+
+#include "theme.h"
 #include <Windows.h>
 
 typedef struct Drawing__ {
@@ -8,5 +10,8 @@ typedef struct Drawing__ {
     HBRUSH accentBrush;
 } Drawing;
 
+void BlokDrawingInit(Drawing *drawing, const SelectColours *colours);
+
+void BlokDrawingFree(Drawing *drawing);
 
 #endif // !__BLOK_DRAWING_H_
