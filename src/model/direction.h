14bf5e6795fe32a504c9d0403af096e76cf046b7
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-09 17:00:07 +0000

    feat(model): add direction enum

--- src/model/direction.h
+++ src/model/direction.h
@@ -1,4 +1,11 @@
 #ifndef __BLOK_DIRECTION_H_
 #define __BLOK_DIRECTION_H_
 
+typedef enum Direction__ {
+    BLOK_COMPASS_NORTH = 12,
+    BLOK_COMPASS_EAST = 3,
+    BLOK_COMPASS_SOUTH = 6,
+    BLOK_COMPASS_WEST = 9,
+} Direction;
+
 #endif // !__BLOK_DIRECTION_H_
