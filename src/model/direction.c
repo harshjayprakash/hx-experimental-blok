4d6f84e2d81d3721ffa4cb20c58609e67ced3e04
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-09 17:01:18 +0000

    feat(model): add direction to vector2 covert function

--- src/model/direction.c
+++ src/model/direction.c
@@ -1,1 +1,13 @@
 #include "direction.h"
+
+Vector2 BlokCompassToVector2(const Direction direction)
+{
+    switch (direction)
+    {
+    case BLOK_COMPASS_NORTH: return (Vector2){0,-1};
+    case BLOK_COMPASS_EAST: return (Vector2){1, 0};
+    case BLOK_COMPASS_SOUTH: return (Vector2){0, 1};
+    case BLOK_COMPASS_WEST: return (Vector2){-1, 0};
+    default: return (Vector2){0, 0};
+    }
+}
