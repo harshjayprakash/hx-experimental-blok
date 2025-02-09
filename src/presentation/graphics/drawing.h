251ff1c2afb31f9c9c754c8d0621eabfb52984be
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-09 11:52:49 +0000

    feat(ui-gfx): add drawing structure

--- src/presentation/graphics/drawing.h
+++ src/presentation/graphics/drawing.h
@@ -1,4 +1,12 @@
 #ifndef __BLOK_DRAWING_H_
 #define __BLOK_DRAWING_H_
+#include <Windows.h>
+
+typedef struct Drawing__ {
+    HBRUSH backgroundBrush;
+    HBRUSH foregroundBrush;
+    HBRUSH accentBrush;
+} Drawing;
+
 
 #endif // !__BLOK_DRAWING_H_
