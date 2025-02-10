9638087d642316fd77c37f1ca8aff2322ace237e
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-10 11:46:08 +0000

    feat(model): add functions

--- src/model/state.c
+++ src/model/state.c
@@ -1,1 +1,26 @@
 #include "state.h"
+
+void BlokObjectStateInit(
+    ObjectState *objstate, const Vector2 boxPosition, const int scaleSize)
+{
+    if (!objstate) { return; }
+
+    objstate->box.position = boxPosition;
+    objstate->box.size = (Vector2) {scaleSize, scaleSize};
+}
+
+void BlokObjectStateMoveBox(ObjectState *objstate, const Direction direction)
+{
+    if (!objstate) { return; }
+
+    Vector2 vector = BlokCompassToVector2(direction);
+    Vector2 scaled = BlokVector2MultiplyByScalar(vector, objstate->box.size.x);
+    Vector2 newpos = BlokVector2Add(objstate->box.position, scaled);
+
+    BlokVector2Copy(&objstate->box.position, newpos);
+}
+
+void BlokObjectStateFree(ObjectState *objstate)
+{
+    if (!objstate) { return; }
+}
--- src/model/state.h
+++ src/model/state.h
@@ -1,10 +1,18 @@
 #ifndef __BLOK_STATE_H_
 #define __BLOK_STATE_H_
+
+#include "direction.h"
 #include "square.h"
 
 typedef struct ObjectState__ {
     Square box;
 } ObjectState;
 
+void BlokObjectStateInit(
+    ObjectState *objstate, const Vector2 boxPosition, const int scaleSize);
+
+void BlokObjectStateMoveBox(ObjectState *objstate, const Direction direction);
+
+void BlokObjectStateFree(ObjectState *objstate);
 
 #endif // !__BLOK_STATE_H_
