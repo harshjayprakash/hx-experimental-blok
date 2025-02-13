7b7637428d8f944e77494d31933ef9e7a413b422
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-13 21:24:19 +0000

    feat(main): add mutex single instance check

--- src/main.c
+++ src/main.c
@@ -3,5 +3,27 @@
 int WINAPI wWinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
 {
+    HANDLE instanceMutex = CreateMutexW(NULL, TRUE, L"BlokInstance");
+
+    if (!instanceMutex)
+    {
+        (void) MessageBoxW(
+            NULL, L"Mutex creation failed. The program will now exit.", L"Blok", 
+            MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+    {
+        (void) MessageBoxW(
+            NULL, L"An instance of Blok is already running. "
+            "Please close the other instance before starting a new one.", L"Blok",
+            MB_OK | MB_ICONERROR);
+        CloseHandle(instanceMutex);
+        return 1;
+    }
+
+
+    CloseHandle(instanceMutex);
     return 0;
 }
