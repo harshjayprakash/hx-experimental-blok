0896b133d3dca77cb056188f1a1520b44f77acde
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-13 22:41:42 +0000

    feat(core): add lifecycle source files

--- src/core/lifecycle.c
+++ src/core/lifecycle.c
@@ -1,1 +1,20 @@
 #include "lifecycle.h"
+
+void BlokInit(Context *context, HINSTANCE instance, LPWSTR commandLine, DWORD showFlag)
+{
+    if (!context) { return; }
+
+    context->instance = instance;
+    context->commandLine = commandLine;
+    context->showFlag = showFlag;
+}
+
+void BlokRun(Context *context)
+{
+    if (!context) { return; }
+}
+
+void BlokFree(Context *context)
+{
+    if (!context) { return; }
+}
--- src/core/lifecycle.h
+++ src/core/lifecycle.h
@@ -1,4 +1,12 @@
 #ifndef _BLOK_LIFECYCLE_H_
 #define _BLOK_LIFECYCLE_H_
 
+#include "context.h"
+
+void BlokInit(Context *context, HINSTANCE instance, LPWSTR commandLine, DWORD showFlag);
+
+void BlokRun(Context *context);
+
+void BlokFree(Context *context);
+
 #endif // _BLOK_LIFECYCLE_H_
