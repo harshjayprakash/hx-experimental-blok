db9d0d5b232ebf285790d023b8f5f8981691fc3b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 11:40:06 +0000

    feat(core-gfx): add drawing tools structure

--- src/core/gfx/drawtools.h
+++ src/core/gfx/drawtools.h
@@ -1,4 +1,11 @@
 #ifndef _BLOK_DRAWTOOLS_H_
 #define _BLOK_DRAWTOOLS_H_
+#include <Windows.h>
+
+typedef struct _DrawingTools {
+    HBRUSH backgroundBrush;
+    HBRUSH foregroundBrush;
+    HBRUSH accentBrush;
+} DrawingTools;
 
 #endif // _BLOK_DRAWTOOLS_H_
