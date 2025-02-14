2d0bf2ee37d820c7b3ec4c65bc0a204563705f6a
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 12:59:18 +0000

    feat(ui): add window

--- src/presentation/viewport.h
+++ src/presentation/viewport.h
@@ -1,8 +1,11 @@
 #ifndef _BLOK_VIEWPORT_H_
 #define _BLOK_VIEWPORT_H_
 
+#include "frame/window.h"
+#include <Windows.h>
+
 typedef struct _Viewport {
-    int unused;
+    Window window;
 } Viewport;
 
 void BlokViewportInit(Viewport *viewport);
