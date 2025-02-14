f79e105e713059f7029fa00b17337a11ff9758dc
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 10:46:38 +0000

    feat(core): add theme enum

--- src/core/theme.h
+++ src/core/theme.h
@@ -1,4 +1,10 @@
 #ifndef _BLOK_THEME_H_
 #define _BLOK_THEME_H_
+typedef enum _Theme {
+    BLOK_THEME_UNSET = 0,
+    BLOK_THEME_DARK = 1,
+    BLOK_THEME_LIGHT = 2,
+} Theme;
+
 
 #endif // _BLOK_THEME_H_
