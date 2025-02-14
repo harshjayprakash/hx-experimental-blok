8924028619d26b740bb8b11bf526df6a3a0052bf
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 11:35:12 +0000

    feat(core-gfx): implement set colours function

--- src/core/gfx/theme.c
+++ src/core/gfx/theme.c
@@ -1,1 +1,13 @@
 #include "theme.h"
+
+#define __BLOK_LIGHT_DARK(light, dark) \
+    (theme == BLOK_THEME_LIGHT) ? light : dark
+
+void BlokColoursSet(ColourSpace *colours, const Theme theme)
+{
+    if (!colours) { return; }
+
+    colours->background = __BLOK_LIGHT_DARK(BLOK_COLOUR_WHITE, BLOK_COLOUR_BLACK);
+    colours->foreground = __BLOK_LIGHT_DARK(BLOK_COLOUR_BLACK, BLOK_COLOUR_WHITE);
+    colours->accent = __BLOK_LIGHT_DARK(BLOK_COLOUR_BLUE, BLOK_COLOUR_AQUA);
+}
