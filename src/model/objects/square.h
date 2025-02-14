efa9c992901260eee2ed599ec916e8ba4dfbd442
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 13:42:03 +0000

    feat(model-obj): add square struct

---
+++ src/model/objects/square.h
@@ -0,0 +1,11 @@
+#ifndef _BLOK_SQUARE_H_
+#define _BLOK_SQUARE_H_
+
+#include "../maths/vector.h"
+
+typedef struct _Square {
+    VectorII position;
+    VectorII size;
+} Square;
+
+#endif // _BLOK_SQUARE_H_
