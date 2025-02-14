7804270c1e4ed8df1346f1bf57e02290eafe6463
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 12:52:55 +0000

    feat(ui-frame): add window structure

--- src/presentation/frame/window.h
+++ src/presentation/frame/window.h
@@ -1,4 +1,13 @@
 #ifndef _BLOK_WINDOW_H_
 #define _BLOK_WINDOW_H_
+#include <Windows.h>
+
+typedef struct _Window {
+    HWND handle;
+    WNDCLASSEXW klass;
+    LPWSTR klassName;
+    LPWSTR caption;
+    ATOM klassAtomIdx;
+} Window;
 
 #endif // _BLOK_WINDOW_H_
