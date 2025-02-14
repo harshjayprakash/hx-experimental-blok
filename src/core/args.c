840cc7ad744795881319a8bc4a4e221ec83e3490
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 08:27:04 +0000

    feat(core): add process function

--- src/core/args.c
+++ src/core/args.c
@@ -1,1 +1,28 @@
 #include "args.h"
+
+void BlokArgsProcess(LPWSTR commandLine, ArgsInfo *result)
+{
+    if (!commandLine) { return; }
+    if (!result) { return; }
+
+    result->theme = 0;
+
+    int argc = 0;
+    LPWSTR *args = CommandLineToArgvW(commandLine, &argc);
+
+    if (!args) { return; }
+
+    for (int argIdx = 0; argIdx < argc; ++argIdx)
+    {
+        if (_wcsnicmp(args[argIdx], L"--dark-theme", 13*sizeof(unsigned short)))
+        {
+            result->theme = 1;
+        }
+        if (_wcsnicmp(args[argIdx], L"--light-theme", 14*sizeof(unsigned short)))
+        {
+            result->theme = 2;
+        }
+    }
+
+    (void) LocalFree(args);
+}
--- src/core/args.h
+++ src/core/args.h
@@ -1,7 +1,12 @@
 #ifndef _BLOK_ARGS_H_
 #define _BLOK_ARGS_H_
+
+#include <Windows.h>
+
 typedef struct _ArgsInfo {
     int theme;
 } ArgsInfo;
+
+void BlokArgsProcess(LPWSTR commandLine, ArgsInfo *result);
 
 #endif // _BLOK_ARGS_H_
