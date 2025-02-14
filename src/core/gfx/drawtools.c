bf7c8e109da262f96d0f18da46626f534c0a9b51
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 11:40:27 +0000

    feat(core-gfx): add drawing tool lifecycle functions

--- src/core/gfx/drawtools.c
+++ src/core/gfx/drawtools.c
@@ -1,1 +1,26 @@
 #include "drawtools.h"
+
+#define __BLOK_FREE_WINGDI_OBJECT(winGdiObject) \
+    if (winGdiObject)                           \
+    {                                           \
+        (void) DeleteObject(winGdiObject);      \
+    }                                           
+
+void BlokDrawingToolsInit(DrawingTools *tools, const ColourSpace *colours)
+{
+    if (!tools) { return; }
+    if (!colours) { return; }
+
+    tools->backgroundBrush = CreateSolidBrush(colours->background);
+    tools->foregroundBrush = CreateSolidBrush(colours->foreground);
+    tools->accentBrush = CreateSolidBrush(colours->accent);
+}
+
+void BlokDrawingToolsFree(DrawingTools *tools)
+{
+    if (!tools) { return; }
+
+    __BLOK_FREE_WINGDI_OBJECT(tools->backgroundBrush);
+    __BLOK_FREE_WINGDI_OBJECT(tools->foregroundBrush);
+    __BLOK_FREE_WINGDI_OBJECT(tools->accentBrush);
+}
--- src/core/gfx/drawtools.h
+++ src/core/gfx/drawtools.h
@@ -1,5 +1,7 @@
 #ifndef _BLOK_DRAWTOOLS_H_
 #define _BLOK_DRAWTOOLS_H_
+
+#include "theme.h"
 #include <Windows.h>
 
 typedef struct _DrawingTools {
@@ -7,5 +9,9 @@ typedef struct _DrawingTools {
     HBRUSH foregroundBrush;
     HBRUSH accentBrush;
 } DrawingTools;
+
+void BlokDrawingToolsInit(DrawingTools *tools, const ColourSpace *colours);
+
+void BlokDrawingToolsFree(DrawingTools *tools);
 
 #endif // _BLOK_DRAWTOOLS_H_
