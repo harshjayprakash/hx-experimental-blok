67c6d6fa3bf29d2592661631dab5d65e072cd679
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 12:59:50 +0000

    feat(ui): implement functions

--- src/presentation/viewport.c
+++ src/presentation/viewport.c
@@ -1,11 +1,22 @@
 #include "viewport.h"
 
-void BlokViewportInit(Viewport *viewport)
+void BlokViewportInit(Viewport *viewport, HINSTANCE instance)
 {
     if (!viewport) { return; }
+
+    BlokWindowInit(&viewport->window, instance);
 }
 
-void BlokViewportFree(Viewport *viewport)
+void BlokViewportShow(Viewport *viewport, DWORD showFlag)
 {
     if (!viewport) { return; }
+
+    BlokWindowShow(&viewport->window, showFlag);
+}
+
+void BlokViewportFree(Viewport *viewport, HINSTANCE instance)
+{
+    if (!viewport) { return; }
+
+    BlokWindowFree(&viewport->window, instance);
 }
