ee348a7c8e861429abf322c32c5812b9acffb4c4
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 15:31:53 +0000

    feat(ui-events): add functions

--- src/presentation/events/process.c
+++ src/presentation/events/process.c
@@ -1,1 +1,31 @@
 #include "process.h"
+
+void BlokProcessEventOnCreate()
+{
+
+}
+
+void BlokProcessEventOnDestroy()
+{
+
+}
+
+void BlokProcessEventOnPaint(HWND window)
+{
+
+}
+
+void BlokProcessEventOnKeyDown()
+{
+
+}
+
+void BlokProcessEventOnLeftMouseDown()
+{
+
+}
+
+void BlokProcessEventOnResize()
+{
+    
+}
--- src/presentation/events/process.h
+++ src/presentation/events/process.h
@@ -1,4 +1,18 @@
 #ifndef _BLOK_PROCESS_H_
 #define _BLOK_PROCESS_H_
 
+#include <Windows.h>
+
+void BlokProcessEventOnCreate();
+
+void BlokProcessEventOnDestroy();
+
+void BlokProcessEventOnPaint(HWND window);
+
+void BlokProcessEventOnKeyDown();
+
+void BlokProcessEventOnLeftMouseDown();
+
+void BlokProcessEventOnResize();
+
 #endif // _BLOK_PROCESS_H_
