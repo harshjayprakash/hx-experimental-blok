9598a4ac59c11a6a633ac711e9601ecf192ac8d4
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 13:53:48 +0000

    feat(model): add state functions

--- src/model/state.c
+++ src/model/state.c
@@ -1,1 +1,27 @@
 #include "state.h"
+#include "maths/vector.h"
+
+void BlokStateInit(State *state)
+{
+    if (!state) { return; }
+
+    VectorII defaultBoxState = {0, 0};
+    VectorII defaultBoxSize = {15, 15};
+    BlokVectorIICopy(&state->box.size, defaultBoxSize);
+    BlokVectorIICopy(&state->box.position, defaultBoxState);
+}
+
+void BlokStateFree(State *state)
+{
+    if (!state) { return; }
+}
+
+void BlokStateMoveBox(State *state, const Direction direction)
+{
+    if (!state) { return; }
+
+    VectorII vector = BlokDirectionToVector(direction);
+    VectorII scaled = BloKVectorIIMultiply(vector, state->box.size);
+    VectorII newpos = BlokVectorIIAdd(state->box.position, scaled);
+    BlokVectorIICopy(&state->box.position, newpos);
+}
--- src/model/state.h
+++ src/model/state.h
@@ -1,10 +1,17 @@
 #ifndef _BLOK_STATE_H_
 #define _BLOK_STATE_H_
+
+#include "maths/direction.h"
 #include "objects/square.h"
 
 typedef struct _State {
     Square box;
 } State;
 
+void BlokStateInit(State *state);
+
+void BlokStateFree(State *state);
+
+void BlokStateMoveBox(State *state, const Direction direction);
 
 #endif // _BLOK_STATE_H_
