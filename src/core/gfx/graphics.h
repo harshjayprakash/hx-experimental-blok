6886d73e67e03be5d427e9e08f0fb5528fd36d92
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-14 11:52:00 +0000

    feat(core-gfx): add graphics structure

--- src/core/gfx/graphics.h
+++ src/core/gfx/graphics.h
@@ -1,4 +1,13 @@
 #ifndef _BLOK_GRAPHICS_H_
 #define _BLOK_GRAPHICS_H_
 
+#include "theme.h"
+#include "drawtools.h"
+
+typedef struct _Graphics {
+    Theme theme;
+    ColourSpace colours;
+    DrawingTools tools;
+} Graphics;
+
 #endif // _BLOK_GRAPHICS_H_
