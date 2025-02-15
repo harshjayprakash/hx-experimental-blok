a00264e8a69ec9096d14db5b34b9432f3b4bc2f9
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-15 13:04:34 +0000

    feat(model-maths): add dynamic list functions

--- src/model/maths/dynlist.c
+++ src/model/maths/dynlist.c
@@ -1,1 +1,56 @@
 #include "dynlist.h"
+#include <stdlib.h>
+#include <memory.h>
+
+void BlokDynListInit(DynList *list, const long long size)
+{
+    if (!list) { return; }
+
+    list->arr = calloc(size, sizeof(Node));
+
+    if (!list->arr) { return; }
+
+    list->head = -1;
+    list->max = size;
+    list->size = 0;
+}
+
+int BlokDynListIsFull(const DynList *list)
+{
+    if (!list) { return -1; }
+    if (!list->arr) { return -1; }
+
+    return (list->head == list->max);
+}
+
+int BlokDynListIsEmpty(const DynList *list)
+{
+    if (!list) { return -1; }
+    if (!list->arr) { return -1; }
+
+    return (list->head == -1);
+}
+
+int BlokDynListAdd(DynList *list, const Node *node)
+{
+    if (!list) { return -1; }
+    if (!node) { return -1; }
+    if (BlokDynListIsFull(list)) { return -1; }
+
+    ++list->head;
+    ++list->size;
+
+    BlokVectorIICopy(&(list->arr + list->head)->data, node->data);
+
+    return list->head;
+}
+
+void BlokDynListFree(DynList *list)
+{
+    if (!list) { return; }
+
+    if (list->arr != NULL)
+    {
+        free(list->arr);
+    }
+}
--- src/model/maths/dynlist.h
+++ src/model/maths/dynlist.h
@@ -1,5 +1,6 @@
 #ifndef _BLOK_DYNLIST_H_
 #define _BLOK_DYNLIST_H_
+
 #include "vector.h"
 
 typedef struct _Node Node;
@@ -14,5 +15,15 @@ typedef struct _DynList {
     long long size;
     long long max;
 } DynList;
+
+void BlokDynListInit(DynList *list, const long long size);
+
+int BlokDynListIsFull(const DynList *list);
+
+int BlokDynListIsEmpty(const DynList *list);
+
+int BlokDynListAdd(DynList *list, const Node *node);
+
+void BlokDynListFree(DynList *list);
 
 #endif // _BLOK_DYNLIST_H_
