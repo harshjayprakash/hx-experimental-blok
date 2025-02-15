2ee0b087801e81479c57c200cae6d2940ccdaf71
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-15 10:22:08 +0000

    feat(ui): add components

--- src/presentation/viewport.h
+++ src/presentation/viewport.h
@@ -1,12 +1,15 @@
 #ifndef _BLOK_VIEWPORT_H_
 #define _BLOK_VIEWPORT_H_
 
+#include "components/canvas.h"
+#include "components/panel.h"
 #include "frame/window.h"
 #include <Windows.h>
 
 typedef struct _Viewport {
     Window window;
-
+    Canvas canvas;
+    Panel panel;
     int isCanvasLocked;
     int isGridVisible;
 } Viewport;
