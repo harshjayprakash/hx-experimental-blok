601fbd50a061cd8e96be9a2389608bb907178449
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-15 13:04:18 +0000

    feat(model-maths): add dynamic list structures

--- src/model/maths/dynlist.h
+++ src/model/maths/dynlist.h
@@ -1,4 +1,18 @@
 #ifndef _BLOK_DYNLIST_H_
 #define _BLOK_DYNLIST_H_
+#include "vector.h"
+
+typedef struct _Node Node;
+
+typedef struct _Node {
+    VectorII data;
+} Node;
+
+typedef struct _DynList {
+    Node *arr;
+    long long head;
+    long long size;
+    long long max;
+} DynList;
 
 #endif // _BLOK_DYNLIST_H_
