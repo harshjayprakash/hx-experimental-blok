d25f9eacbbd576a83e20ebf6913cf67aac3d8da1
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-15 13:38:50 +0000

    feat(core-debug): add console functions

--- src/core/debug/console.c
+++ src/core/debug/console.c
@@ -1,1 +1,39 @@
 #include "console.h"
+#include <Windows.h>
+
+void BlokConsoleInit(DebugConsole *con)
+{
+    if (!con) { return; }
+
+    con->conResult = AllocConsole();
+
+    if (con->conResult == 0)
+    {
+        (void) MessageBoxW(
+            0, L"Console Initialisation Failed.", L"Blok", MB_OK | MB_ICONERROR);
+        return;
+    }
+
+    con->fileResult = freopen_s(&con->output, "CONOUT$", "w", stdout);
+
+    if (con->fileResult != 0)
+    {
+        (void) MessageBoxW(
+            0, L"Opening StdOut Failed.", L"Blok", MB_OK | MB_ICONERROR);
+    }
+}
+
+void BlokConsoleFree(DebugConsole *con)
+{
+    if (!con) { return; }
+
+    if (con->fileResult == 0)
+    {
+        (void) fclose(stdout);
+    }
+
+    if (con->conResult != 0)
+    {
+        (void) FreeConsole();
+    }
+}
--- src/core/debug/console.h
+++ src/core/debug/console.h
@@ -1,4 +1,16 @@
 #ifndef _BLOK_CONSOLE_H_
 #define _BLOK_CONSOLE_H_
 
+#include <stdio.h>
+
+typedef struct _DebugConsole {
+    int conResult;
+    int fileResult;
+    FILE *output;
+} DebugConsole;
+
+void BlokConsoleInit(DebugConsole *con);
+
+void BlokConsoleFree(DebugConsole *con);
+
 #endif // _BLOK_CONSOLE_H_
