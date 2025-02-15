71bfb01e2eed8b0b6e29fe14d2677b3b14454f20
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-15 11:25:01 +0000

    feat(ui-comp): add canvas update functions

--- src/presentation/components/canvas.c
+++ src/presentation/components/canvas.c
@@ -1,1 +1,16 @@
 #include "canvas.h"
+
+void BlokCanvasUpdate(Canvas *canvas, const RECT *windowRegion)
+{
+    if (!canvas) { return; }
+    if (!windowRegion) { return; }
+
+    (void) CopyRect(&canvas->region, windowRegion);
+
+    VectorII size = {
+        canvas->region.right - canvas->region.left, 
+        canvas->region.bottom - canvas->region.top
+    };
+
+    BlokVectorIICopy(&canvas->size, size);
+}
--- src/presentation/components/canvas.h
+++ src/presentation/components/canvas.h
@@ -1,10 +1,14 @@
 #ifndef _BLOK_CANVAS_H_
 #define _BLOK_CANVAS_H_
 
+#include "../../model/maths/vector.h"
 #include <Windows.h>
 
 typedef struct _Canvas {
     RECT region;
+    VectorII size;
 } Canvas;
+
+void BlokCanvasUpdate(Canvas *canvas, const RECT *windowRegion);
 
 #endif // _BLOK_CANVAS_H_
