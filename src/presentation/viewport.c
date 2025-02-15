e4a6600a72518e071a51d99294db48234e1e91e8
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-15 11:26:40 +0000

    feat(ui): invoke component update functions

--- src/presentation/viewport.c
+++ src/presentation/viewport.c
@@ -5,6 +5,12 @@ void BlokViewportInit(Viewport *viewport, HINSTANCE instance)
     if (!viewport) { return; }
 
     BlokWindowInit(&viewport->window, instance);
+
+    (void) GetClientRect(viewport->window.handle, &viewport->surface);
+
+    VectorII panelSize = {0, 40}, panelMargin = {0, 0};
+    BlokPanelUpdateEx(&viewport->panel, &viewport->surface, &panelSize, &panelMargin);
+    BlokCanvasUpdate(&viewport->canvas, &viewport->surface);
 }
 
 void BlokViewportShow(Viewport *viewport, DWORD showFlag)
