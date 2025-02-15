1d2f92dc7cff594031967e6db6c0f15d68ddb902
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-15 11:24:43 +0000

    feat(ui-comp): add panel update functions

--- src/presentation/components/panel.c
+++ src/presentation/components/panel.c
@@ -1,1 +1,31 @@
 #include "panel.h"
+
+void BlokPanelUpdateEx(
+    Panel *panel, const RECT *windowRegion, const VectorII *size, const VectorII *margin)
+{
+    if (!panel) { return; }
+    if (!windowRegion) { return; }
+
+    if (size != (VectorII *) 0)
+    {
+        BlokVectorIICopy(&panel->size, *size);
+    }
+
+    if (margin != (VectorII *) 0)
+    {
+        BlokVectorIICopy(&panel->margin, *margin);
+    }
+
+    panel->region.left = windowRegion->left + panel->margin.x;
+    panel->region.top = (windowRegion->bottom - panel->size.y) - panel->margin.y;
+    panel->region.right = windowRegion->right - panel->margin.x;
+    panel->region.bottom = windowRegion->bottom - panel->margin.y;
+}
+
+void BlokPanelUpdate(Panel *panel, const RECT *windowRegion)
+{
+    if (!panel) { return; }
+    if (!windowRegion) { return; }
+
+    BlokPanelUpdateEx(panel, windowRegion, NULL, NULL);
+}
--- src/presentation/components/panel.h
+++ src/presentation/components/panel.h
@@ -1,10 +1,19 @@
 #ifndef _BLOK_PANEL_H_
 #define _BLOK_PANEL_H_
 
+#include "../../model/maths/vector.h"
 #include <Windows.h>
 
 typedef struct _Panel {
     RECT region;
+    VectorII margin;
+    VectorII position;
+    VectorII size;
 } Panel;
+
+void BlokPanelUpdateEx(
+    Panel *panel, const RECT *windowRegion, const VectorII *size, const VectorII *margin);
+
+void BlokPanelUpdate(Panel *panel, const RECT *windowRegion);
 
 #endif // _BLOK_PANEL_H_
