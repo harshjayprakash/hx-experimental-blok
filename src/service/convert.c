4d8b243277c6cc2b94615533015e72b9c8fb50ad
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-16 11:58:22 +0000

    feat(svc): add rect, coord, vec2 helper functions

--- src/service/convert.c
+++ src/service/convert.c
@@ -1,1 +1,26 @@
 #include "convert.h"
+
+VectorII BlokConvertCoordV(const COORD coord)
+{
+    return (VectorII) {coord.X, coord.Y};
+}
+
+COORD BlokConvertVectorCoord(const VectorII vec)
+{
+    return (COORD) {vec.x, vec.y};
+}
+
+RECT BlokConvertVectorRect(const VectorII position, const VectorII size)
+{
+    return (RECT) {position.x, position.y, position.x+size.x, position.y+size.y};
+}
+
+VectorII BlokConvertRectPositionV(const RECT rc)
+{
+    return (VectorII) {rc.top, rc.left};
+}
+
+VectorII BlokConvertRectSizeV(const RECT rc)
+{
+    return (VectorII) {rc.right-rc.left, rc.bottom-rc.top};
+}
--- src/service/convert.h
+++ src/service/convert.h
@@ -1,4 +1,17 @@
 #ifndef _BLOK_CONVERT_H_
 #define _BLOK_CONVERT_H_
 
+#include "../model/maths/vector.h"
+#include <Windows.h>
+
+VectorII BlokConvertCoordV(const COORD coord);
+
+COORD BlokConvertVectorCoord(const VectorII vec);
+
+RECT BlokConvertVectorRect(const VectorII position, const VectorII size);
+
+VectorII BlokConvertRectPositionV(const RECT rc);
+
+VectorII BlokConvertRectSizeV(const RECT rc);
+
 #endif // _BLOK_CONVERT_H_
