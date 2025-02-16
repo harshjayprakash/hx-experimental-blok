6c04c568bc5742992fbbad922f505687144925c3
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-16 11:43:32 +0000

    feat(ui-frame): add font and coordinates

--- src/presentation/frame/window.h
+++ src/presentation/frame/window.h
@@ -9,7 +9,9 @@ typedef struct _Window {
     LPWSTR klassName;
     LPWSTR caption;
     ATOM klassAtomIdx;
+    HFONT font;
     RECT region;
+    COORD mouse;
 } Window;
 
 void BlokWindowInit(Window *window, HINSTANCE instance);
