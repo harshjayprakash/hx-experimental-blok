606af0f2f1b84610d9aabca880d874f60588f41b
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-17 19:20:29 +0000

    feat(ui): init random and components

--- src/presentation/viewport.c
+++ src/presentation/viewport.c
@@ -1,8 +1,12 @@
 #include "viewport.h"
+#include <strsafe.h>
+#include <time.h>
 
 void BlokViewportInit(Viewport *viewport, HINSTANCE instance)
 {
     if (!viewport) { return; }
+
+    srand((unsigned int) time(0));
 
     BlokWindowInit(&viewport->window, instance);
 
@@ -22,10 +26,35 @@ void BlokViewportInit(Viewport *viewport, HINSTANCE instance)
 
     (void) GetClientRect(viewport->window.handle, &viewport->region);
 
-    COORD panelSize = {300, 40};
+    COORD panelSize = {500, 40};
     COORD panelMargin = {0, 0};
     BlokPanelUpdateEx(&viewport->panel, &viewport->region, &panelSize, &panelMargin);
     BlokCanvasUpdate(&viewport->canvas, &viewport->region);
+
+    POINT coordTextPos = {viewport->panel.region.left +10, viewport->panel.region.top +10};
+    SIZE coordTextMgn = {0, 0};
+    SIZE coordTextSize = {100, 20};
+    BlokTextUpdateEx(
+        &viewport->coordinatesText, &coordTextPos, &coordTextSize, &coordTextMgn);
+    (void) StringCbPrintfW(viewport->coordinatesText.data, 60, L"(0, 0)");
+
+    POINT clearBtnPos = {
+        viewport->coordinatesText.region.right+10, 
+        viewport->coordinatesText.region.top
+    };
+    SIZE clearBtnMgn = {0, 0};
+    SIZE clearBtnSz = {60, 20};
+    BlokButtonUpdateEx(
+        &viewport->clearAllButton, &clearBtnPos, &clearBtnSz, &clearBtnMgn);
+    (void) StringCbPrintfW(viewport->clearAllButton.text, 60, L"&Clear All");
+
+    POINT genBtnPos = {
+        viewport->clearAllButton.region.right+10, 
+        viewport->clearAllButton.region.top
+    };
+    SIZE genBtnSz = {130, 20};
+    BlokButtonUpdateEx(&viewport->generateButton, &genBtnPos, &genBtnSz, &clearBtnMgn);
+    (void) StringCbPrintfW(viewport->generateButton.text, 60, L"Generate &Obstructive");
 }
 
 void BlokViewportShow(Viewport *viewport, DWORD showFlag)
