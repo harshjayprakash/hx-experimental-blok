8b5a9d8a7a2b2f01ed6bec4225ba7c992dd5a49e
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-17 19:19:54 +0000

    feat(ui): add additional controls

--- src/presentation/viewport.h
+++ src/presentation/viewport.h
@@ -1,6 +1,8 @@
 #ifndef _BLOK_VIEWPORT_H_
 #define _BLOK_VIEWPORT_H_
 
+#include "controls/button.h"
+#include "controls/text.h"
 #include "components/canvas.h"
 #include "components/panel.h"
 #include "frame/window.h"
@@ -8,14 +10,20 @@
 
 typedef struct _Viewport {
     Window window;
-    Canvas canvas;
-    Panel panel;
     RECT region;
     COORD mousePos;
     HFONT font;
+
+    Canvas canvas;
     int isCanvasLocked;
     int isGridVisible;
+
+    Panel panel;
     int isInterfaceVisible;
+    Text coordinatesText;
+    Button clearAllButton;
+    Button generateButton;
+
     int x, y;
 } Viewport;
 
