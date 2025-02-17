f0717d1b297f7301b810220c5d4198414049369a
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-17 19:17:17 +0000

    feat(ui-ctl): add text structure and update functions

--- src/presentation/controls/text.c
+++ src/presentation/controls/text.c
@@ -1,1 +1,37 @@
 #include "text.h"
+
+void BlokTextUpdateEx(Text *text, const POINT *point, const SIZE *size, const SIZE *margin)
+{
+    if (!text) { return; }
+
+    if (point != (POINT *) 0)
+    {
+        text->position.x = point->x;
+        text->position.y = point->y;
+    }
+
+    if (size != (SIZE *) 0)
+    {
+        text->size.cx = size->cx;
+        text->size.cy = size->cy;
+    }
+
+    if (margin != (SIZE *) 0)
+    {
+        text->margin.cx = margin->cx;
+        text->margin.cy = margin->cy;
+    }
+
+    text->region.left = text->position.x + text->margin.cx;
+    text->region.top = text->position.y + text->margin.cy;
+    text->region.right = (text->position.x + text->size.cx) - text->margin.cx;
+    text->region.bottom = (text->position.y + text->size.cy) - text->margin.cx;
+}
+
+void BlokTextUpdate(Text *text, const POINT *position)
+{
+    if (!text) { return; }
+    if (!position) { return; }
+
+    BlokTextUpdateEx(text, position, (SIZE *) 0, (SIZE *) 0);
+}
--- src/presentation/controls/text.h
+++ src/presentation/controls/text.h
@@ -1,4 +1,21 @@
 #ifndef _BLOK_TEXT_H_
 #define _BLOK_TEXT_H_
 
+#include <Windows.h>
+
+#define _BLOK_TEXT_SIZE 60
+
+typedef struct _Text {
+    WCHAR data[_BLOK_TEXT_SIZE];
+    RECT region;
+    SIZE size;
+    SIZE margin;
+    POINT position;
+} Text;
+
+void BlokTextUpdateEx(
+    Text *text, const POINT *point, const SIZE *size, const SIZE *margin);
+
+void BlokTextUpdate(Text *text, const POINT *position);
+
 #endif // _BLOK_TEXT_H_
