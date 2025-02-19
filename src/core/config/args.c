fa30a726158ea85df9c46920de368661c3405d67
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-02-19 15:52:29 +0000

    feat(core-config): add scale in both directions

--- src/core/config/args.c
+++ src/core/config/args.c
@@ -4,6 +4,7 @@
 #define __BLOK_ARGS_MODE_UNSET 0x0
 #define __BLOK_ARGS_MODE_CUSTOM_SCALE_X 0x10
 #define __BLOK_ARGS_MODE_CUSTOM_SCALE_Y 0x20
+#define __BLOK_ARGS_MODE_CUSTOM_SCALE_A 0x30
 #define __BLOK_DEFAULT_SCALE 15
 
 void BlokArgsProcess(LPWSTR commandLine, ArgsInfo *result)
@@ -45,6 +46,12 @@ void BlokArgsProcess(LPWSTR commandLine, ArgsInfo *result)
             result->scaleY = (val != 0) ? val : __BLOK_DEFAULT_SCALE;
             mode = __BLOK_ARGS_MODE_UNSET;
         }
+        if (mode == __BLOK_ARGS_MODE_CUSTOM_SCALE_A)
+        {
+            int val = _wtoi(args[argIdx]);
+            result->scaleY = result->scaleX = (val != 0) ? val : __BLOK_DEFAULT_SCALE;
+            mode = __BLOK_ARGS_MODE_UNSET;
+        }
 
         if (_wcsnicmp(args[argIdx], L"--scale-x", 10*sizeof(unsigned short)) == 0)
         {
@@ -54,8 +61,10 @@ void BlokArgsProcess(LPWSTR commandLine, ArgsInfo *result)
         {
             mode = __BLOK_ARGS_MODE_CUSTOM_SCALE_Y;
         }
-
-
+        if (_wcsnicmp(args[argIdx], L"--scale", 8*sizeof(unsigned short)) == 0)
+        {
+            mode = __BLOK_ARGS_MODE_CUSTOM_SCALE_A;
+        }
     }
 
     (void) LocalFree(args);
